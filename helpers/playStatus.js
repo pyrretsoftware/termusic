28c9b510894ae58134fb786c58e3149afc654dc7
axellse <mail@axell.me>
2024-07-29 21:03:13 +0200

    add idling image style configuration

--- helpers/playStatus.js
+++ helpers/playStatus.js
@@ -1,3 +1,4 @@
+import { config } from "../snippets/config.js";
 import { reWriteCommandText } from "../ui/uiManagers/player.js";
 import { clearBar } from "../ui/utils/clearBar.js";
 import { moveCursorPos } from "../ui/utils/moveCursorPos.js";
@@ -10,10 +11,12 @@ const widthChars = 46
 export let outputWritten = false;
 export let currentSongReport 
 
+const idlingImageStyle = config['idlingImageStyle']
+
 export function setPlayStatus(type, playStatus) { //playStatus can either be a string or a video object
     if (type == 'report') {
         if (playStatus['special'] == 'idling') {
-            changeRpcStatus('', 'https://apis.axell.me/termusic/v1/idling-image', '', true)
+            changeRpcStatus('', 'https://apis.axell.me/termusic/v1/idling-images/from-style/' + idlingImageStyle, '', true)
         } else {
             changeRpcStatus(playStatus["title"], playStatus["thumbnail"], playStatus["id"])
             currentSongReport = playStatus
