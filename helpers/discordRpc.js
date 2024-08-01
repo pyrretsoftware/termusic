f4c57da2253fe0d3f16ed8f6b633545c5ecdf0b3
axellse <mail@axell.me>
2024-08-01 22:24:14 +0200

    fix discordrpc button

--- helpers/discordRpc.js
+++ helpers/discordRpc.js
@@ -9,10 +9,12 @@ if (process.argv[2] == 'launch') {
 
     ep.on('connected', () => {
         connected = true
-        setPlayStatus("report", {
-            'special' : 'idling'
-        })
         setPlayStatus('important', 'Connected to Discord.')
+        setTimeout(() => {
+            setPlayStatus("report", {
+                'special' : 'idling'
+            })
+        }, 500) //wait a little before starting discordrpc
     })
     ep.on('disconnected', () => connected = false)
 }
@@ -22,17 +24,22 @@ export function changeRpcStatus(title, thumbnail, id, idling = false) {
         setPlayStatus('important_err', 'Could not connect to Discord.')
     }
 
-    ep.setActivity({
+    let activity = {
         details: (idling == false ? ("Listening to " + title) : 'Idling'),
         assets: {
             large_image: thumbnail + '?cacheStop=' + Math.floor(Math.random() * 500)
         },
         timestamps: { start: new Date() },
-        buttons : (idling == true ? [] : [
+    }
+
+    if (!idling) {
+        activity['buttons'] = [
             {
                 label: "View song",
                 url: "https://termusic.axell.me/?s=" + id
             }
-        ]),
-    })
+        ]
+    }
+
+    ep.setActivity(activity)
 }
