63b668bf3e24c121a4c5aa87a1c7f36ab70563e1
axellse <mail@axell.me>
2024-08-17 16:11:37 +0200

    put less strain on yt api during tests

--- tests/ytSearch.test.js
+++ tests/ytSearch.test.js
@@ -2,13 +2,13 @@ import { searchYoutube } from "../helpers/ytScrape.js"
 import test from "node:test";
 import assert from 'node:assert';
 
+const result = await searchYoutube('california girls')
+
 test('ytScrape does not throw an error', async () => {
-    const result = await searchYoutube('california girls')
     if (!result) {
         assert.fail('ytScrape threw an error.')
     }
 })
 test('ytScrape returns the california girls music video', async () => {
-    const result = await searchYoutube('california girls')
     assert.equal(result['id'], 'F57P9C4SAW4')
 })
